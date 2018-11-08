6b2b55e060d87c008ca3a6cb5b4c7f4980a4a770
moonspiritacm <yefanbupt@gmail.com>
2018-11-08 18:48:46 +0800

    1

---
+++ src/com/moonspirit/leetcode/Template.java
@@ -0,0 +1,24 @@
+package com.moonspirit.leetcode;
+
+public class Template {
+
+	public static void main(String[] args) {
+		int[] testTable = { 0, 1, 2, 3, 4, 5, 6, 10, };
+		for (int ito : testTable) {
+			// test(ito);
+		}
+	}
+//		private static void test(int ito) {
+//			Solution solution = new Solution();
+//			int rtn;
+//			long begin = System.currentTimeMillis();
+//			rtn = solution.numTrees(ito);
+//			long end = System.currentTimeMillis();
+//			System.out.println(ito + ": rtn=" + rtn);
+//			System.out.println();
+//			System.out.println("耗时：" + (end - begin) + "ms");
+//			System.out.println("-------------------");
+//			}
+//			}
+
+}
