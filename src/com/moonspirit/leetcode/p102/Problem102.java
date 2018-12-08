4daf3f42602019cec9818692af0b50e75ba09572
“moonspiritacm <yefanbupt@gmail.com>
2018-12-08 18:20:43 +0800

    [Leetcode 102](https://leetcode.com/problems/binary-tree-level-order-traversal/)

--- src/com/moonspirit/leetcode/p102/Problem102.java
+++ src/com/moonspirit/leetcode/p102/Problem102.java
@@ -3,7 +3,6 @@ package com.moonspirit.leetcode.p102;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
