a8f4b61af58e30543d87fde89229bc93abae7574
moonspiritacm <yefanbupt@gmail.com>
2019-01-11 17:06:06 +0800

    1

--- src/com/moonspirit/leetcode/p438/Problem438.java
+++ src/com/moonspirit/leetcode/p438/Problem438.java
@@ -3,49 +3,20 @@ package com.moonspirit.leetcode.p438;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-/**
- * @ClassName      Problem001
- * @Description    [Leetcode 001](https://leetcode.com/problems/two-sum/) 数据结构——映射/哈希表
- * @author         moonspirit
- * @date           2019年1月10日 上午1:30:13
- * @version        1.0.0
- */
 public class Problem438 {
-	public static int[] stringToIntegerArray(String input) {
-		input = input.trim();
-		input = input.substring(1, input.length() - 1).trim();
-		if (input.length() == 0)
-			return new int[0];
-
-		String[] parts = input.split(",");
-		int[] output = new int[parts.length];
-		for (int i = 0; i < parts.length; i++)
-			output[i] = Integer.parseInt(parts[i].trim());
-		return output;
-	}
-
-	public static String integerArrayToString(int[] input) {
-		if (input.length == 0)
-			return "[]";
-
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < input.length; i++)
-			sb.append(input[i]).append(", ");
-		return "[" + sb.substring(0, sb.length() - 2) + "]";
-	}
-
 	public static void main(String[] args) throws IOException {
-		Scanner in = new Scanner(Paths.get("src/com/moonspirit/leetcode/p001/Problem001.txt"), "UTF-8");
-		SolutionB solution = new SolutionB();
+		Scanner in = new Scanner(Paths.get("src/com/moonspirit/leetcode/p438/Problem438.txt"), "UTF-8");
+		SolutionA solution = new SolutionA();
 
 		long begin = System.currentTimeMillis();
 		while (in.hasNextLine()) {
-			int[] nums = stringToIntegerArray(in.nextLine());
-			int target = Integer.parseInt(in.nextLine());
-			System.out.println(integerArrayToString(solution.twoSum(nums, target)));
+			String s = in.nextLine();
+			String p = in.nextLine();
+			System.out.println(solution.findAnagrams(s, p));
 		}
 		long end = System.currentTimeMillis();
 		System.out.println("耗时：" + (end - begin) + "ms");
@@ -54,23 +25,51 @@ public class Problem438 {
 	}
 }
 
-class Solution {
+class SolutionA {
 	public List<Integer> findAnagrams(String s, String p) {
 		List<Integer> res = new ArrayList<>();
-		char[] array = s.toCharArray();
-		int[] map = new int[256];
-		int length = p.length();
+		char[] sArray = s.toCharArray();
+		char[] pArray = p.toCharArray();
+		int[] target = new int[26];
+		for (int i = 0; i < pArray.length; i++) {
+			target[pArray[i] - 'a']++;
+		}
 
-		for (int i = 0; i < length; i++)
-			map[p.charAt(i)]++;
-		for (int i = 0; i <= array.length - length; i++) {
-			int[] tmp = new int[256];
-			for (int j = 0; j < length; j++) {
-				tmp[array[i + j]]++;
+		for (int i = 0; i <= sArray.length - pArray.length; i++) {
+			int[] map = new int[26];
+			for (int j = 0; j < pArray.length; j++) {
+				map[sArray[i + j] - 'a']++;
 			}
-			if (map == tmp) {
+			if (Arrays.equals(target, map)) {
 				res.add(i);
 			}
+		}
+		return res;
+	}
+}
+
+class SolutionB {
+	public List<Integer> findAnagrams(String s, String p) {
+		List<Integer> res = new ArrayList<>();
+		char[] sArray = s.toCharArray();
+		char[] pArray = p.toCharArray();
+		int[] target = new int[26];
+		int[] map = new int[26];
+		for (int i = 0; i < pArray.length; i++) {
+			target[pArray[i] - 'a']++;
+		}
+
+		int left = 0;
+		int right = 0;
+		while (right < sArray.length) {
+			map[sArray[right] - 'a']++;
+			if (right - left + 1 == pArray.length) {
+				if (Arrays.equals(target, map)) {
+					res.add(left);
+				}
+				map[sArray[left++] - 'a']--;
+			}
+			right++;
 		}
 		return res;
 	}
