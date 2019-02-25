3842858b976e7bbaa113b8b311016a10cee7f8ea
moonspiritacm <yefanbupt@gmail.com>
2019-02-25 17:08:44 +0800

    [Leetcode 056] 线性扫描

--- src/com/moonspirit/leetcode/p056/Problem056.java
+++ src/com/moonspirit/leetcode/p056/Problem056.java
@@ -2,81 +2,85 @@ package com.moonspirit.leetcode.p056;
 
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
- * @ClassName      Problem001
- * @Description    [Leetcode 001](https://leetcode.com/problems/two-sum/) 数据结构——映射/哈希表
+ * @ClassName      Problem056
+ * @Description    [Leetcode 056](https://leetcode.com/problems/merge-intervals/) 线性扫描
  * @author         moonspirit
- * @date           2019年1月10日 上午1:30:13
+ * @date           2019年2月25日 下午4:33:41
  * @version        1.0.0
  */
 public class Problem056 {
-	public static int[] stringToIntegerArray(String input) {
-		input = input.trim();
-		input = input.substring(1, input.length() - 1).trim();
-		if (input.length() == 0)
-			return new int[0];
-
-		String[] parts = input.split(",");
-		int[] output = new int[parts.length];
-		for (int i = 0; i < parts.length; i++)
-			output[i] = Integer.parseInt(parts[i].trim());
-		return output;
-	}
-
-	public static String integerArrayToString(int[] input) {
-		if (input.length == 0)
-			return "[]";
-
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < input.length; i++)
-			sb.append(input[i]).append(", ");
-		return "[" + sb.substring(0, sb.length() - 2) + "]";
-	}
-
 	public static void main(String[] args) throws IOException {
-		Scanner in = new Scanner(Paths.get("src/com/moonspirit/leetcode/p001/Problem001.txt"), "UTF-8");
-		SolutionB solution = new SolutionB();
+		Scanner in = new Scanner(Paths.get("src/com/moonspirit/leetcode/p056/Problem056.txt"), "UTF-8");
+		// Solution solution = new Solution();
 
-		long begin = System.currentTimeMillis();
-		while (in.hasNextLine()) {
-			int[] nums = stringToIntegerArray(in.nextLine());
-			int target = Integer.parseInt(in.nextLine());
-			System.out.println(integerArrayToString(solution.twoSum(nums, target)));
-		}
-		long end = System.currentTimeMillis();
-		System.out.println("耗时：" + (end - begin) + "ms");
+		/*
+		 * long begin = System.currentTimeMillis(); while (in.hasNextLine()) { int[]
+		 * nums = stringToIntegerArray(in.nextLine()); int target =
+		 * Integer.parseInt(in.nextLine());
+		 * System.out.println(integerArrayToString(solution.twoSum(nums, target))); }
+		 * long end = System.currentTimeMillis(); System.out.println("耗时：" + (end -
+		 * begin) + "ms");
+		 */
 
 		in.close();
 	}
 }
 
+class Interval {
+	int start;
+	int end;
+
+	Interval() {
+		start = 0;
+		end = 0;
+	}
+
+	Interval(int s, int e) {
+		start = s;
+		end = e;
+	}
+}
+
 class Solution {
-	private void sort(int[] a, int i, int j) {
-		if(l >= h)
-			return;
-		
-		int key = a[l];
-		while(h--) {
-			while(a[j--] < key) {
-				int tmp = a[i];
-				a[i] = a[j];
-				a[j] = tmp;
+	public static void qSort(List<Interval> a, int low, int high) {
+		int l = low;
+		int h = high;
+		Interval key = a.get(low);
+
+		while (l < h) {
+			while (l < h && a.get(h).start >= key.start)
+				h--;
+			a.set(l, a.get(h));
+
+			while (l < h && a.get(l).start <= key.start)
+				l++;
+			a.set(h, a.get(l));
+		}
+		a.set(h, key);
+		if (low < l)
+			qSort(a, low, l - 1);
+		if (h < high)
+			qSort(a, h + 1, high);
+	}
+
+	public List<Interval> merge(List<Interval> intervals) {
+		if (intervals == null || intervals.size() < 2)
+			return intervals;
+
+		qSort(intervals, 0, intervals.size() - 1);
+		List<Interval> res = new ArrayList<>();
+		for (Interval interval : intervals) {
+			if (res.isEmpty() || interval.start > res.get(res.size() - 1).end) {
+				res.add(interval);
+			} else {
+				res.get(res.size() - 1).end = Math.max(interval.end, res.get(res.size() - 1).end);
 			}
 		}
+		return res;
 	}
-	
-    public List<Interval> merge(List<Interval> intervals) {
-        List<Interval> res = new ArrayList<>();
-        for(int i = 1; i < intervals.size(); i++) {
-            if(intervals.get(i).start > intervals.get(i-1)) {
-                res.add(new Interval())
-            }
-        }
-        
-    }
-    com.moonspirit.leetcode.p001
 }
