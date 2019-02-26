f258db70631ac2c6ee144fcb047a7d2ac622e997
moonspiritacm <yefanbupt@gmail.com>
2019-02-26 22:55:11 +0800

    [Leetcode 208] 字典树

---
+++ src/com/moonspirit/leetcode/Solution.java
@@ -0,0 +1,10 @@
+package com.moonspirit.leetcode;
+
+public class Solution {
+	public int JumpFloorII(int target) {
+		if (target == 1)
+			return 1;
+
+		return 2 * JumpFloorII(target - 1);
+	}
+}
--- src/com/moonspirit/leetcode/p208/Problem208.java
+++ src/com/moonspirit/leetcode/p208/Problem208.java
@@ -2,6 +2,8 @@ package com.moonspirit.leetcode.p208;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -24,6 +26,9 @@ public class Problem208 {
 		obj.insert("app");
 		System.out.println(obj.search("app"));
 		System.out.println(obj.startsWith("app"));
+		System.out.println(obj.search(""));
+		obj.insert("");
+		System.out.println(obj.startsWith(""));
 		long end = System.currentTimeMillis();
 		System.out.println("耗时：" + (end - begin) + "ms");
 
@@ -31,63 +36,102 @@ public class Problem208 {
 	}
 }
 
+/*
+ * class TrieNode { boolean isWord; TrieNode[] next;
+ * 
+ * public TrieNode() { next = new TrieNode[26]; } }
+ * 
+ * class Trie { TrieNode root;
+ * 
+ * public Trie() { root = new TrieNode(); }
+ * 
+ * public void insert(String input) { if (input == null || input.length() == 0)
+ * return;
+ * 
+ * char[] chs = input.toCharArray(); TrieNode curr = root; for (char ch : chs) {
+ * if (curr.next[ch - 'a'] == null) { curr.next[ch - 'a'] = new TrieNode(); }
+ * curr = curr.next[ch - 'a']; } curr.isWord = true; }
+ * 
+ * public boolean search(String input) { if (input == null || input.length() ==
+ * 0) return true;
+ * 
+ * char[] chs = input.toCharArray(); TrieNode curr = root; for (char ch : chs) {
+ * if (curr.next[ch - 'a'] == null) { return false; } curr = curr.next[ch -
+ * 'a']; } return curr.isWord; }
+ * 
+ * public boolean startsWith(String input) { if (input == null || input.length()
+ * == 0) return true;
+ * 
+ * char[] chs = input.toCharArray(); TrieNode curr = root; for (char ch : chs) {
+ * if (curr.next[ch - 'a'] == null) { return false; } curr = curr.next[ch -
+ * 'a']; } return true; } }
+ */
+
 class TrieNode {
 	boolean isWord;
-	TrieNode[] next;
+	Map<Character, TrieNode> next;
 
 	public TrieNode() {
-		next = new TrieNode[26];
+		next = new HashMap<>();
 	}
 }
 
 class Trie {
-	private TrieNode root;
+	TrieNode root;
 
 	public Trie() {
 		root = new TrieNode();
 	}
 
-	public void insert(String word) {
-		if (word == null || word.length() == 0)
+	public void insert(String input) {
+		if (input == null)
 			return;
+		if (input.length() == 0) {
+			root.isWord = true;
+			return;
+		}
 
+		char[] chs = input.toCharArray();
 		TrieNode curr = root;
-		char[] chs = word.toCharArray();
 		for (char ch : chs) {
-			if (curr.next[ch - 'a'] == null) {
-				curr.next[ch - 'a'] = new TrieNode();
+			if (!curr.next.containsKey(ch)) {
+				curr.next.put(ch, new TrieNode());
 			}
-			curr = curr.next[ch - 'a'];
+			curr = curr.next.get(ch);
 		}
 		curr.isWord = true;
 	}
 
-	public boolean search(String word) {
-		if (word == null || word.length() == 0)
+	public boolean search(String input) {
+		if (input == null)
+			return false;
+		if (input.length() == 0 && root.isWord)
 			return true;
 
+		char[] chs = input.toCharArray();
 		TrieNode curr = root;
-		char[] chs = word.toCharArray();
 		for (char ch : chs) {
-			if (curr.next[ch - 'a'] == null) {
+			if (!curr.next.containsKey(ch)) {
 				return false;
 			}
-			curr = curr.next[ch - 'a'];
+			curr = curr.next.get(ch);
 		}
 		return curr.isWord;
 	}
 
-	public boolean startsWith(String prefix) {
-		if (prefix == null || prefix.length() == 0)
+	public boolean startsWith(String input) {
+		if (input == null)
+			return false;
+		if (input.length() == 0)
 			return true;
 
+		char[] chs = input.toCharArray();
 		TrieNode curr = root;
-		char[] chs = prefix.toCharArray();
 		for (char ch : chs) {
-			if (curr.next[ch - 'a'] == null) {
+			if (!curr.next.containsKey(ch)) {
 				return false;
 			}
-			curr = curr.next[ch - 'a'];
+			curr = curr.next.get(ch);
 		}
 		return true;
 	}
