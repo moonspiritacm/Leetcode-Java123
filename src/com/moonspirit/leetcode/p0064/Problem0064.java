f106f39de47b858e7392f19a94b349b634ad8989
moonspiritacm <yefanbupt@gmail.com>
2019-03-05 01:32:08 +0800

    [Leetcode 0064] 动态规划

--- src/com/moonspirit/leetcode/p0064/Problem0064.java
+++ src/com/moonspirit/leetcode/p0064/Problem0064.java
@@ -29,7 +29,80 @@ public class Problem0064 {
 	}
 }
 
+/**
+ * @ClassName      SolutionA
+ * @Description    普通递归，重复求解子问题，时间复杂度 O(2^n)
+ * @author         moonspirit
+ * @date           2019年3月5日 上午1:31:01
+ * @version        1.0.0
+ */
 class SolutionA {
+	private int helper(int[][] grid, int m, int n) {
+		if (m == 1 && n == 1)
+			return grid[m - 1][n - 1];
+
+		if (m == 1)
+			return grid[m - 1][n - 1] + helper(grid, m, n - 1);
+		else if (n == 1)
+			return grid[m - 1][n - 1] + helper(grid, m - 1, n);
+		else
+			return grid[m - 1][n - 1] + Math.min(helper(grid, m, n - 1), helper(grid, m - 1, n));
+	}
+
+	public int minPathSum(int[][] grid) {
+		if (grid == null || grid.length == 0)
+			return 0;
+
+		int m = grid.length;
+		int n = grid[0].length;
+		return helper(grid, m, n);
+	}
+}
+
+/**
+ * @ClassName      SolutionB
+ * @Description    记忆化递归，时间复杂度 O(mn)
+ * @author         moonspirit
+ * @date           2019年3月5日 上午1:31:20
+ * @version        1.0.0
+ */
+class SolutionB {
+	private int[][] dp;
+
+	private int helper(int[][] grid, int m, int n) {
+		if (m == 1 && n == 1)
+			return grid[m - 1][n - 1];
+		if (dp[m][n] != 0)
+			return dp[m][n];
+
+		if (m == 1)
+			dp[m][n] = grid[m - 1][n - 1] + helper(grid, m, n - 1);
+		else if (n == 1)
+			dp[m][n] = grid[m - 1][n - 1] + helper(grid, m - 1, n);
+		else
+			dp[m][n] = grid[m - 1][n - 1] + Math.min(helper(grid, m, n - 1), helper(grid, m - 1, n));
+		return dp[m][n];
+	}
+
+	public int minPathSum(int[][] grid) {
+		if (grid == null || grid.length == 0)
+			return 0;
+
+		int m = grid.length;
+		int n = grid[0].length;
+		dp = new int[m + 1][n + 1];
+		return helper(grid, m, n);
+	}
+}
+
+/**
+ * @ClassName      SolutionC
+ * @Description    二维动态规划，时间复杂度 O(mn)
+ * @author         moonspirit
+ * @date           2019年3月5日 上午1:31:33
+ * @version        1.0.0
+ */
+class SolutionC {
 	public int minPathSum(int[][] grid) {
 		if (grid == null || grid.length == 0)
 			return 0;
@@ -51,24 +124,31 @@ class SolutionA {
 	}
 }
 
-class SolutionB {
+/**
+ * @ClassName      SolutionD
+ * @Description    二维动态规划，滚动数组优化空间复杂度
+ * @author         moonspirit
+ * @date           2019年3月5日 上午1:31:40
+ * @version        1.0.0
+ */
+class SolutionD {
 	public int minPathSum(int[][] grid) {
 		if (grid == null || grid.length == 0)
 			return 0;
 
 		int m = grid.length;
 		int n = grid[0].length;
-		int[][] dp = new int[2][n];
-		dp[0][0] = grid[0][0];
+		int[] dp = new int[n];
+		dp[0] = grid[0][0];
 		for (int j = 1; j < n; j++) {
-			dp[0][j] = dp[0][j - 1] + grid[0][j];
+			dp[j] = dp[j - 1] + grid[0][j];
 		}
 		for (int i = 1; i < m; i++) {
-			dp[i % 2][0] = dp[(i - 1) % 2][0] + grid[i][0];
+			dp[0] = dp[0] + grid[i][0];
 			for (int j = 1; j < n; j++) {
-				dp[i % 2][j] = Math.min(dp[(i - 1) % 2][j], dp[i % 2][j - 1]) + grid[i][j];
+				dp[j] = Math.min(dp[j], dp[j - 1]) + grid[i][j];
 			}
 		}
-		return dp[(m - 1) % 2][n - 1];
+		return dp[n - 1];
 	}
 }
